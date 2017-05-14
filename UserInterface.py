835ab4261f2736a52fc494640b99b55b5d3e96ca
ares97 <slowinskir@wp.pl>
2017-05-14 18:31:15 +0100

    added creating chart from file data

--- UserInterface.py
+++ UserInterface.py
@@ -3,9 +3,14 @@ import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
+import matplotlib.animation as animation
+from matplotlib import style
 
 import tkinter as tk
 from tkinter import *
+
+style.use("dark_background")
+
 
 LARGE_FONT = ("Verdana", 12)
 NASDAQ = ["TSLA.US", "AAPL.US", "AMD.US", "NVDA.US"];
@@ -15,6 +20,22 @@ stockMarkets = {'NASDAQ OMX': NASDAQ,
                 'NYSE Euronext(US)': NYSEEuronextUS,
                 'Japan Exchange Group': JapanExchangeGroup}
 
+f = Figure(figsize=(5, 5), dpi=100)
+a = f.add_subplot(111)
+
+def animate(i):
+    pullData = open("sampleStockData.txt","r").read()
+    dataList = pullData.split('\n')
+    xList = []
+    yList = []
+    for eachLine in dataList:
+        if len(eachLine)>1:
+            x,y = eachLine.split(',')
+            xList.append(int(x))
+            yList.append(int(y))
+
+    a.clear()
+    a.plot(xList,yList)
 
 class StockAnalyser(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -41,7 +62,6 @@ class StockAnalyser(tk.Tk):
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
-
 
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -71,18 +91,16 @@ class StartPage2(tk.Frame):
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-        f = Figure(figsize=(5, 5), dpi=100)
-        a = f.add_subplot(111)
-        a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
-
         canvas = FigureCanvasTkAgg(f, self)
         canvas.show()
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=TRUE)
 
         toolbar = NavigationToolbar2TkAgg(canvas,self)
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas._tkcanvas.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
         toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=TRUE)
+
 
 
 app = StockAnalyser()
+ani = animation.FuncAnimation(f, animate, interval=1000)
 app.mainloop()
